e88dba67bc86fa0975565159f0b64552d33852c1
michal6677 <michalmarek10@wp.pl>
2015-02-26 14:45:31 +0100

    First controller

--- projekt_angular/app/app.js
+++ projekt_angular/app/app.js
@@ -2,11 +2,8 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+controller('MainCtrl',function($scope){
+    $scope.hello = 'world';
+});
